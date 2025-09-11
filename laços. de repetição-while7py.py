3f4b424046939d1a595e36186f65d94c9cd8479b
Byank-C-S-Lima <byankchrystinny01@gmail.com>
2025-09-11 10:33:35 -0300

    Add files via upload

---
+++ laços. de repetição-while7py.py
@@ -0,0 +1,8 @@
+thanos = 1.50
+aranha = 1.10
+anos= 0
+while thanos > aranha:
+    thanos += 0.02
+    aranha += 0.03
+    anos += 1
+print("Vai demorar ", anos, " anos para a aranha ser maior que o thanos")
